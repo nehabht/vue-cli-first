5c350750585b96e706863227aa1c3b7034b572cd
nehabht <ne.bhatti2@icloud.com>
2022-05-02 16:58:31 +0200

    initial commit and add title

---
+++ vue-cli-first/src/components/Title.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>La mia prima app con Vue CLI!</h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Title'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1{
+    color: cornflowerblue;
+}
+
+</style>
